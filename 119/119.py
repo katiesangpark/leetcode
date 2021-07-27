0f9e2dc20974b3c7ed782c1da3ad906b6c9acfc3
Katie Park <katie.sang.park@gmail.com>
2021-07-26 21:48:34 -0400

    update actual code

--- 119/119.py
+++ 119/119.py
@@ -1,1 +1,8 @@
-print ("hi")
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        row = [1]
+        
+        for x in range(1, rowIndex +1):
+            row.append(row[x -1] * (rowIndex - x +1)// x)
+        return row
+        
